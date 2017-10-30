87155d8b4211a4451da4db986e58549651fcde9f
Robby <ta7382@gmail.com>
2017-10-30 15:48:24 +0800

    新增 - 影片任務

--- src/app/container/mission/video/video.component.ts
+++ src/app/container/mission/video/video.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-video',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VideoComponent implements OnInit {
 
-  constructor() { }
+  public videoName: String = '';
+  public videoExp: String = '';
+  public videoUrl: any = 'Z5ztoOdXkEY';
+  public videoUrlFrm: any;
+
+  constructor(
+    private domSanitizer: DomSanitizer
+  ) { }
 
   ngOnInit() {
+
+    // 處理網址問題
+    this.ConvertUrl();
+  }
+
+  public ConvertUrl() {
+    this.videoUrlFrm = this.domSanitizer
+      .bypassSecurityTrustResourceUrl(`https://www.youtube.com/embed/${this.videoUrl}`);
   }
 
 }
