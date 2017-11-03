caf79b79ee0764d1e2d4479557331ad275f7705f
Robby <ta7382@gmail.com>
2017-11-03 16:35:07 +0800

    更新 - 個人首頁

--- src/app/container/user/user.component.ts
+++ src/app/container/user/user.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { UserService } from '../../service/user/user.service';
 import { Cookie } from 'ng2-cookies/ng2-cookies';
+
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
@@ -9,14 +10,16 @@ import { Cookie } from 'ng2-cookies/ng2-cookies';
   providers: [UserService]
 })
 export class UserComponent implements OnInit {
-  public missions:any;
-  public missionlist=[];
+
+  public missions: any;
+  public missionlist = [];
   public result: any;
   public userdata: any;
+
   constructor(
     private router: Router,
     private userService: UserService
-) { }
+  ) { }
 
   ngOnInit() {
     this.userdata = JSON.parse(Cookie.get('userCookie'));
@@ -24,25 +27,23 @@ export class UserComponent implements OnInit {
       this.missioncheck();
     }
   }
+
   public async missioncheck() {
     let body = "username=" + this.userdata.childusername;
     await this.userService.Getmission(body).subscribe(
       result => {
         this.missions = result;
-        this.missionadd()            
-      }
-    )
+        this.missionadd();
+      });
   }
+
   public async missionadd() {
-    for(let i=0;i<this.missions.length;i++){
+    for (let i = 0; i < this.missions.length; i++) {
       let body = this.missions[i].missionid;
       await this.userService.Getmissionlist(body).subscribe(
         result => {
           this.missionlist.push(result[0])
-        }
-      )
+        });
     }
-    
-    //this.missionlist = temp;
   }
 }
